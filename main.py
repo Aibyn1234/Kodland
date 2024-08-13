957e8591390945f2003bde9cffccb76e0bb850da
Aibyn1234 <aibyn.nugmanov@yandex.ru>
2024-08-13 21:42:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
-import random
+import discord
+from discord.ext import commands
 
-chars = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-pass_len = int(input("Введите длину пароля "))
-result = ""
+intents = discord.Intents.default()
+intents.message_content = True
 
-for i in range(pass_len):
-    result += random.choice(chars)
+bot = commands.Bot(command_prefix='$', intents=discord.Intents.default())
 
-print("ваш пароль:", result)
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def sum(ctx, a, b):
+    await ctx.send(int(a)+ int(b))
+
+@bot.command()
+async def sub(ctx, a, b):
+    await ctx.send(int(a)- int(b))
+
+@bot.command()
+async def multi(ctx, a, b):
+    await ctx.send(int(a)* int(b))
+
+@bot.command()
+async def divi(ctx, a, b):
+    await ctx.send(int(a)/ int(b))
+
+@bot.command()
+async def bothelp(ctx):
+    help_text = (
+            "bothelp - команда Help показывает все существующие команды\n"
+            "heh - если написать просто heh, бот будет смеяться\n"
+            "sum - сложение двух чисел\n"
+            "sub - вычитание двух чисел\n"
+            "multi - умножение двух чисел\n"
+            "divi - деление двух чисел\n"
+        )
+    await ctx.send(help_text)
+
+
+
+bot.run("")
